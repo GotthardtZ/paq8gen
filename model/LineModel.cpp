1c8efa5c6c78a9446cbea1fa3d1e2017beb02500
Zoltán Gotthardt <gotty@freemail.hu>
2021-01-02 22:16:51 +0100

    Added 10 contexts and 2 mixer context sets to LineModel

--- model/LineModel.cpp
+++ model/LineModel.cpp
@@ -3,7 +3,7 @@
 
 LineModel::LineModel(Shared* const sh, const uint64_t size) : 
   shared(sh),
-  cm(sh, size, nCM, 74)
+  cm(sh, size, nCM, 64)
 {}
 
 void LineModel::mix(Mixer &m) {
@@ -11,28 +11,112 @@ void LineModel::mix(Mixer &m) {
   if (bpos == 0) {
     INJECT_SHARED_pos
     INJECT_SHARED_c1
+    INJECT_SHARED_c4
+    INJECT_SHARED_c8
+
+    uint8_t g = c1;
+    if (g >= 128) {
+      //utf8 code points (weak context)
+      if ((g & 0xf8u) == 0xf0) g = 1;
+      else if ((g & 0xf0u) == 0xe0) g = 2;
+      else if ((g & 0xe0u) == 0xc0) g = 3;
+      else if ((g & 0xc0u) == 0x80) g = 4; //the rest of the values
+      else if (g == 0xff) g = 5;
+      else g = c1 & 0xf0u;
+    }
+    else if (g >= '0' && g <= '9') g = '0';
+    else if (g >= 'a' && g <= 'z') g = 'a';
+    else if (g >= 'A' && g <= 'Z') g = 'A';
+    else if (g == ' ') g = 6;
+    else if (g == '.') g = 7;
+    else if (g == '>') g = 8;
+    else g = 0;
+    groups = groups << 8U | g;
+
     const uint8_t RH = CM_USE_RUN_STATS | CM_USE_BYTE_HISTORY;
     const auto isNewline = c1 == NEW_LINE || c1 == 0;
     if (isNewline) { // a new line has just started (or: zero in asciiz or in binary data)
+      nl2 = nl1;
       nl1 = pos;
+      line0 = 0;
+      groups = 0;
       firstChar = 256;
+    }
+    else {
+      line0 = combine64(line0, c1);
     }
 
     uint64_t col = pos - nl1;
     if (col == 1) {
-      firstChar = c1 >= 'A' && c1 <= 'Z' ? 'A' : c1;
+      firstChar = c1;
+      if ('A' <= firstChar && firstChar <= 'Z')
+        firstChar = 'A';
+    }
+    INJECT_SHARED_buf
+    const uint8_t cAbove = buf[nl2 + col];
+    const uint8_t pCAbove = buf[nl2 + col - 1];
+
+    const auto isNewLineStart = col == 0 && nl2 > 0;
+    const auto isPrevCharMatchAbove = c1 == pCAbove && col != 0 && nl2 != 0;
+    const uint32_t aboveCtx = cAbove << 1U | uint32_t(isPrevCharMatchAbove);
+    if (isNewLineStart) {
+      lineMatch = 0; //first char not yet known = nothing to match
+      groups = 0;
+    }
+    else if (lineMatch >= 0 && isPrevCharMatchAbove) {
+      lineMatch = min(lineMatch + 1, maxLineMatch); //match continues
+    }
+    else {
+      lineMatch = -1; //match does not continue
     }
 
     uint64_t i = 0;
 
-    // modeling line content per column
+    cm.set(RH, hash(++i, line0));
+
+    // context: matches with the previous line
+    if (firstChar != 'A' && lineMatch >= 0) {
+      cm.set(RH, hash(++i, cAbove, lineMatch));
+    }
+    else {
+      cm.skip(RH); i++;
+    }
+
+    if (firstChar != 'A') {
+      cm.set(RH, hash(++i, aboveCtx, c1));
+      cm.set(RH, hash(++i, col << 9U | aboveCtx, c1));
+      const int lineLength = nl1 - nl2;
+      cm.set(RH, hash(++i, nl1 - nl2, col, aboveCtx));
+      cm.set(RH, hash(++i, nl1 - nl2, col, aboveCtx, c1));
+      cm.set(RH, hash(++i, groups & 0xFF));
+      cm.set(RH, hash(++i, groups & 0xFFFF));
+    }
+    else {
+      cm.skipn(RH, 6); 
+      i += 6;
+    }
+
+    // modeling line content per column (and NEW_LINE is some extent)
     cm.set(RH, hash(++i, col));
     cm.set(RH, hash(++i, col, c1));
 
-    // content of lines, paragraphs
-    cm.set(RH, hash(++i, nl1)); //chars occurring in this paragraph (order 0)
-    cm.set(RH, hash(++i, firstChar)); //chars occurring in a paragraph that began with firstChar (order 0)
+    if (firstChar == 'A') {
+      cm.set(RH, hash(++i, col, c4));
+      cm.set(RH, hash(++i, col, c4, c8));
+    }
+    else {
+      cm.skipn(RH, 2);
+      i += 2;
+    }
+
+    cm.set(RH, hash(++i, nl1)); // chars occurring in this paragraph (order 0)
+    cm.set(RH, hash(++i, 0)); // order 0 context (seconding to the normalmodel one)
+
     assert(i == nCM);
   }
   cm.mix(m);
+  
+  m.set(groups & 0xff, 256);
+  m.set(firstChar == 'A' ? 2 : firstChar == '>' ? 1 : 0, 3);
 }
+
--- model/LineModel.hpp
+++ model/LineModel.hpp
@@ -10,12 +10,18 @@
  */
 class LineModel {
 private:
-  static constexpr int nCM = 4;
+  static constexpr int nCM = 14;
 public:
     static constexpr int MIXERINPUTS = nCM * (ContextMap2::MIXERINPUTS + ContextMap2::MIXERINPUTS_RUN_STATS + ContextMap2::MIXERINPUTS_BYTE_HISTORY); // 28
-    static constexpr int MIXERCONTEXTS = 0;
-    static constexpr int MIXERCONTEXTSETS = 0;
+    static constexpr int MIXERCONTEXTS = 256 + 3;
+    static constexpr int MIXERCONTEXTSETS = 2;
+
+    static constexpr int maxLineMatch = 16;
+    int lineMatch{}; /**< the length of match of the current line vs the previous line */
+    uint64_t groups{}; /**< 8 last character categories */
+    uint64_t nl2{};
     uint64_t nl1{};
+    uint64_t line0{};
     int firstChar{};
 private:
     Shared * const shared;
